a7cd657ac01a7aba76ad42ff1d4af97e60a21e3f
Yuki Omori <yukiomori4211@gmail.com>
2018-02-09 16:09:56 +0900

    Propsを使ってHello World

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import Greeting from './Greeting.jsx';
+
+class App extends Component {
+  render() {
+    return (<Greeting name={'Bob'} />);
+  }
+}
+
+export default App;
