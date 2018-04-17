537a3e3e001241d120fb8ac1390d0495af845fe1
Yuki Omori <yukiomori4211@gmail.com>
2018-04-17 14:08:58 +0900

    firebaseinfoを読み込めるように修正

---
+++ src/settings/account.js
@@ -0,0 +1,10 @@
+const firebaseAuthInfo = {
+  apiKey: 'AIzaSyA9jPAQ98zKEcgjiA6JwUWu9BjC4qGEVOw',
+  authDomain: 'faaculty-1c2c0.firebaseapp.com',
+  databaseURL: 'https://faaculty-1c2c0.firebaseio.com',
+  projectId: 'faaculty-1c2c0',
+  storageBucket: 'faaculty-1c2c0.appspot.com',
+  messagingSenderId: '809468660388',
+};
+
+export default firebaseAuthInfo;
